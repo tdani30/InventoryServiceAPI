27bc04d1d467f950c8cd157deb931b316ba20251
tdani30 <syedtalha30@gmail.com>
2020-05-10 17:15:53 +0400

    Add project files.

---
+++ AuthorizeAttribute.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace InventoryService
+{
+    public class AuthorizeAttribute : TypeFilterAttribute
+    {
+        public AuthorizeAttribute() : base(typeof(UnauthorizedFilter))
+        {
+            //Empty constructor
+        }
+
+    }
+    public class UnauthorizedFilter : IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            Microsoft.Extensions.Primitives.StringValues authTokens;
+            bool IsAuthenticated = context.HttpContext.User.Identity.IsAuthenticated;
+           // context.HttpContext.Request.Headers.TryGetValue("authToken", out1 authTokens);
+            var _token = context.HttpContext.Request.Headers["Authorization"].Count() !=0 ? context.HttpContext.Request.Headers["Authorization"][0]:"";
+            if (!IsAuthenticated)
+            {
+                //if (context.HttpContext.Request.IsAjaxRequest())
+                //{
+                context.HttpContext.Response.StatusCode = (int)HttpStatusCode.Unauthorized; //Set HTTP 403 - JRozario
+                context.HttpContext.Response.Headers.Add("authToken", _token);
+                context.HttpContext.Response.Headers.Add("AuthStatus", "NotAuthorized");
+
+                context.HttpContext.Response.StatusCode = (int)HttpStatusCode.Forbidden;
+                context.HttpContext.Response.HttpContext.Features.Get<IHttpResponseFeature>().ReasonPhrase = "Not Authorized";
+                context.Result = new JsonResult("NotAuthorized")
+                {
+                    Value = new
+                    {
+                        Status = "Error",
+                        Message = "Invalid Token"
+                    },
+                };
+            }
+            else
+            {
+                context.HttpContext.Response.Headers.Add("AuthStatus", "Authorized");
+                context.HttpContext.Response.Headers.Add("authToken", _token);
+                context.HttpContext.Response.Headers.Add("storeAccessiblity", "Authorized");
+
+                return;
+            }
+        }
+
+        //public void OnAuthorization(AuthorizationFilterContext filterContext)
+        //{
+        //    if (filterContext != null)
+        //    {
+        //        Microsoft.Extensions.Primitives.StringValues authTokens;
+        //        filterContext.HttpContext.Request.Headers.TryGetValue("authToken", out authTokens);
+
+        //        var _token = authTokens.FirstOrDefault();
+
+        //        if (_token != null)
+        //        {
+        //            string authToken = _token;
+        //            if (authToken != null)
+        //            {
+        //                if (IsValidToken(authToken))
+        //                {
+        //                    filterContext.HttpContext.Response.Headers.Add("authToken", authToken);
+        //                    filterContext.HttpContext.Response.Headers.Add("AuthStatus", "Authorized");
+
+        //                    filterContext.HttpContext.Response.Headers.Add("storeAccessiblity", "Authorized");
+
+        //                    return;
+        //                }
+        //                else
+        //                {
+        //                    filterContext.HttpContext.Response.Headers.Add("authToken", authToken);
+        //                    filterContext.HttpContext.Response.Headers.Add("AuthStatus", "NotAuthorized");
+
+        //                    filterContext.HttpContext.Response.StatusCode = (int)HttpStatusCode.Forbidden;
+        //                    filterContext.HttpContext.Response.HttpContext.Features.Get<IHttpResponseFeature>().ReasonPhrase = "Not Authorized";
+        //                    filterContext.Result = new JsonResult("NotAuthorized")
+        //                    {
+        //                        Value = new
+        //                        {
+        //                            Status = "Error",
+        //                            Message = "Invalid Token"
+        //                        },
+        //                    };
+        //                }
+
+        //            }
+
+        //        }
+        //        else
+        //        {
+        //            filterContext.HttpContext.Response.StatusCode = (int)HttpStatusCode.ExpectationFailed;
+        //            filterContext.HttpContext.Response.HttpContext.Features.Get<IHttpResponseFeature>().ReasonPhrase = "Please Provide authToken";
+        //            filterContext.Result = new JsonResult("Please Provide authToken")
+        //            {
+        //                Value = new
+        //                {
+        //                    Status = "Error",
+        //                    Message = "Please Provide authToken"
+        //                },
+        //            };
+        //        }
+        //    }
+        //}
+
+        public bool IsValidToken(string authToken)
+        {
+            //validate Token here  
+            return true;
+        }
+
+
+    }
+
+   
+}
---
+++ Controllers/TokenController.cs
@@ -0,0 +1,81 @@
+﻿using InventoryService.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        public IConfiguration _configuration;
+        private readonly InventoryContext _context;
+
+        public TokenController(IConfiguration config, InventoryContext context)
+        {
+            _configuration = config;
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult Post(UserInfo _userData)
+        {
+
+            if (_userData != null && _userData.Email != null)
+            {
+                var user = GetUser(_userData.Email, _userData.Password);
+
+                if (user != null)
+                {
+                    //create claims details based on the user information
+                    var claims = new[] {
+                    new Claim(JwtRegisteredClaimNames.Sub, _configuration["Jwt:Subject"]),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                    new Claim(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString()),
+                    new Claim("Id", user.UserId.ToString()),
+                    new Claim("FirstName", user.FirstName),
+                    new Claim("LastName", user.LastName),
+                    new Claim("UserName", user.UserName),
+                    new Claim("Email", user.Email)
+                   };
+
+                    var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+
+                    var signIn = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                    var token = new JwtSecurityToken(_configuration["Jwt:Issuer"], _configuration["Jwt:Audience"], claims, expires: DateTime.UtcNow.AddMinutes(1), signingCredentials: signIn);
+                   
+                    var sdssd = new JwtSecurityTokenHandler().WriteToken(token);
+                    HttpContext.Request.Headers.Add("authToken", sdssd);
+                    return Ok(sdssd);
+                }
+                else
+                {
+                    return BadRequest("Invalid credentials");
+                }
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        private UserInfo GetUser(string email, string password)
+        {
+            UserInfo lora = new UserInfo();
+            lora.UserId = 1;
+            lora.FirstName = "Syed";
+            lora.LastName = "talha";
+            lora.UserName = "syedtalha30";
+            lora.Email = "syedtalha30@gmail.com";
+            return lora;
+        }
+    }
+}
